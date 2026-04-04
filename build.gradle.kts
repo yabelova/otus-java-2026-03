e3ca9f534a42c59eef6af12c8a1a11639f3b33fc
Yulia Belova <gigia@ya.ru>
2026-04-04 20:19:31 +0300

    hw01: remove unused plugins and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
-plugins {
-    alias(libs.plugins.spring.dependency.management)
-    alias(libs.plugins.spring.boot) apply false
-    alias(libs.plugins.sonarlint) apply false
-    alias(libs.plugins.spotless) apply false
-}
-
 allprojects {
     apply(plugin = "java")
-    apply(plugin = "io.spring.dependency-management")
 
     group = "ru.otus"
 
     repositories {
-        mavenLocal()
         mavenCentral()
     }
 
